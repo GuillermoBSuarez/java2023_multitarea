67733ec0a20976cd80bce1d6779140d6c76a6cf0
tarde <tarde@A13E31>
2023-11-20 16:44:31 +0100

    más y examen

---
+++ Examen/src/examen/preguntas_static.java
@@ -0,0 +1,22 @@
+package examen;
+
+public class preguntas_static {
+
+}
+
+class Test{
+	static int a;
+	int b;
+	static void m1() {
+		a++;
+		b++;				// Un elemento no estático no puede acceder a uno estático.
+	}
+	void m2() {
+		a++;
+		Test.m1();
+	}
+	static void m3() {
+		m1();
+		m2();				// Un elemento no estático no puede acceder a uno estático.
+	}
+}
